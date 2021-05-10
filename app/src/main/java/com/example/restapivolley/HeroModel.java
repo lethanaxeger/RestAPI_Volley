7529d616f13b73ecc58598a4902a97ad31e480c0
lethanfadlil <fadlillah_bashir_28rpl@student.smktelkom-mlg.sch.id>
2021-05-10 19:35:37 +0700

    Letsgooo

---
+++ app/src/main/java/com/example/restapivolley/HeroModel.java
@@ -0,0 +1,31 @@
+package com.example.restapivolley;
+
+public class HeroModel {
+    private final String heroName;
+    private final String heroAttr;
+    private final String heroAttack;
+    private final String heroImg;
+
+    public HeroModel(String heroName, String heroAttr, String heroAttack, String heroImg) {
+        this.heroName = heroName;
+        this.heroAttr = heroAttr;
+        this.heroAttack = heroAttack;
+        this.heroImg = heroImg;
+    }
+
+    public String getHeroName() {
+        return heroName;
+    }
+
+    public String getHeroAttr() {
+        return heroAttr;
+    }
+
+    public String getHeroAttack() {
+        return heroAttack;
+    }
+
+    public String getHeroImg() {
+        return heroImg;
+    }
+}
